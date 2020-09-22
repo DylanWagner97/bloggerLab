538b986f74ca6c6ed5c22329e081b89bfff5269c
DylanWagner97 <dylanwagner9597@gmail.com>
2020-09-22 02:09:31 +0000

    forgot I was using github, added ability for menu, menu options and one controller for multiple views, added all requirements for lab

--- app.js
+++ app.js
@@ -4,23 +4,27 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes  = require('./routes/index');
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('viewsh', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_module/jquery/dist'));
+app.use('/css', express.static(__dirname + '/public/stylesheets'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var ctrlHome = require('../controllers/home');
-var ctrlBlog = require('../controllers/blog');
+var controllers = require('../controllers/home');
+var controller = require('../controllers/blog');
 
 /* Set routes for home and blog */
-router.get('/', ctrlHome);
-router.get('/blog', ctrlBlog.blog);
+router.get('/', controllers.home);
+router.get('/blogList', controller.blogList);
+router.get('/blogAdd', controller.blogAdd);
 
 module.exports = router;
